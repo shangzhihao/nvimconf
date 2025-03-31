926d334b2ef3b8f7f2ad77e782fb0628d41d379e
shangzhihao <zhihao.shang@gmail.com>
2025-03-31 12:01:20 +0800

    fitten for completion

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,23 +1,23 @@
 return {
-    "folke/which-key.nvim",
-    dependencies = {
-        { "nvim-tree/nvim-web-devicons" },
-    },
-    event = "VeryLazy",
-    config = function()
-        local which_key = require("which-key")
-        which_key.setup({
-            delay = 800,
-            win = {
-                padding = { 1, 1, 1, 1 },
-                border = "single",
-                height = 10,
-                wo = { winblend = 20 }
-            },
-        })
-        require("nvim-web-devicons").setup()
-        require("config.key-binding").setup()
-    end,
-    opts = {},
-    keys = {},
+	"folke/which-key.nvim",
+	dependencies = {
+		{ "nvim-tree/nvim-web-devicons" },
+	},
+	event = "VeryLazy",
+	config = function()
+		local which_key = require("which-key")
+		which_key.setup({
+			delay = 800,
+			win = {
+				padding = { 1, 1, 1, 1 },
+				border = "single",
+				height = 10,
+				wo = { winblend = 20 },
+			},
+		})
+		require("nvim-web-devicons").setup()
+		require("config.key-binding").setup()
+	end,
+	opts = {},
+	keys = {},
 }
